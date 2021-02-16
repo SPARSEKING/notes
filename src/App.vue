657945e7eed2c0e30c763f55a5f5958d072b83e1
Kirill Gavrilchik <gavrilchikkirill6@gmail.com>
2021-02-16 21:48:50 +0300

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 <template>
   <header>
     <div class="navbar">
       <div class="container">
         <div class="navbar-content">
           <div class="logo">NOTES-APP</div>
-          <ul class="navbar-list">
-            <li class="navbar-item" v-for="link in links" :key="link.title">
+          <ul class="navbar-list" v-if="auth">
+            <li class="navbar-item" v-for="link in linksAuth" :key="link.title">
+              <router-link class="navbar-link" :to="link.url">{{
+                link.title
+              }}</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-list" v-else>
+            <li class="navbar-item" v-for="link in linksLogout" :key="link.title">
               <router-link class="navbar-link" :to="link.url">{{
                 link.title
               }}</router-link>
@@ -25,7 +33,21 @@ export default defineComponent({
   name: "App",
   data() {
     return {
-      links: [
+      linksAuth: [
+        {
+          title: "Note",
+          url: "/note"
+        },
+        {
+          title: "About",
+          url: "/"
+        },
+        {
+          title: "Sign Out",
+          url: "/authorization"
+        }
+      ],
+      linksLogout: [
         {
           title: "About",
           url: "/"
@@ -33,7 +55,7 @@ export default defineComponent({
         {
           title: "Sign In",
           url: "/authorization"
-        }
+        },
       ],
       auth: false,
       user: {
@@ -47,30 +69,21 @@ export default defineComponent({
     return {
       auth: this.auth,
       user: this.user,
-      logIn: this.logIn,
-      logOut: this.logOut
+      logIn: this.logIn
     };
   },
   methods: {
     logIn() {
       if (this.user.login != "" && this.user.password != "") {
         localStorage.setItem("auth", "true");
-        this.links.unshift({
-          title: "Note",
-          url: "/note"
-        });
-        this.links[1].title = "Log Out";
       }
       this.user.login = "";
       this.user.password = "";
       this.user.show = false;
-    },
-    logOut() {
-      if (this.user.login === "" && this.user.password === "") {
-        localStorage.removeItem("auth");
-        delete this.links[0];
-      }
     }
+  },
+  created() {
+    this.auth = (localStorage.getItem("auth") === "true");
   }
 });
 </script>
